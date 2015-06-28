bdd4f53c6f9811f3e7eb2630755fd31c74a4cfe0
batwalrus76 <chancebatwalrus@gmail.com>
2015-06-28 14:10:20 -0400

    Chapter 5 blocks is now working

--- src/hello-world.cu
+++ src/hello-world.cu
@@ -8,58 +8,89 @@
 
 #include <stdio.h>
 
-#define ARRAY_SIZE 256
+#define N 16
+#define BLOCK_SIZE 16
+
+#define ARRAY_SIZE BLOCK_SIZE
 #define ARRAY_SIZE_IN_BYTES (sizeof(unsigned int) * (ARRAY_SIZE))
 
 /* Declare  statically four arrays of ARRAY_SIZE each */
 unsigned int cpu_block[ARRAY_SIZE];
-unsigned int cpu_thread[ARRAY_SIZE];
+//unsigned int cpu_thread[ARRAY_SIZE];
 //unsigned int cpu_warp[ARRAY_SIZE];
 //unsigned int cpu_calc_thread[ARRAY_SIZE];
 
+__global__ 
+void hello(char *a, int *b) 
+{
+	a[threadIdx.x] += b[threadIdx.x];
+}
 
 __global__
-void what_is_my_id(unsigned int * block, unsigned int * thread)
+void hello2(int * block)
 {
 	const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-	block[thread_idx] = blockIdx.x;
-	thread[thread_idx] = threadIdx.x;
+	block[thread_idx] = threadIdx.x;
 }
 
 void main_sub0()
 {
+	char a[N] = "Hello \0\0\0\0\0\0";
+	int b[N] = {15, 10, 6, 0, -11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+	char *ad;
+	int *bd;
+	const int csize = N*sizeof(char);
+	const int isize = N*sizeof(int);
+
+	printf("%s", a);
+
+	cudaMalloc( (void**)&ad, csize ); 
+	cudaMalloc( (void**)&bd, isize ); 
+	cudaMemcpy( ad, a, csize, cudaMemcpyHostToDevice ); 
+	cudaMemcpy( bd, b, isize, cudaMemcpyHostToDevice ); 
+	
+	dim3 dimBlock( BLOCK_SIZE, 1 );
+	dim3 dimGrid( 1, 1 );
+	hello<<<dimGrid, dimBlock>>>(ad, bd);
+	cudaMemcpy( a, ad, csize, cudaMemcpyDeviceToHost ); 
+	cudaFree( ad );
+	cudaFree( bd );
+	
+	printf("%s\n", a);
+	sleep(1);
+}
+
+void main_sub1()
+{
 
 	/* Declare pointers for GPU based params */
-	unsigned int *gpu_block;
-	unsigned int *gpu_thread;
+	int *gpu_block;
 
 	cudaMalloc((void **)&gpu_block, ARRAY_SIZE_IN_BYTES);
-	cudaMalloc((void **)&gpu_thread, ARRAY_SIZE_IN_BYTES);
 	cudaMemcpy( cpu_block, gpu_block, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice );
-	cudaMemcpy( cpu_thread, gpu_thread, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice );
 
-	const unsigned int num_blocks = ARRAY_SIZE/128;
-	const unsigned int num_threads = ARRAY_SIZE/num_blocks;
+	dim3 dimBlock( BLOCK_SIZE/2, 1 );
+	dim3 dimGrid( 2, 1 );
 
 	/* Execute our kernel */
-	what_is_my_id<<<num_blocks, num_threads>>>(gpu_block, gpu_thread);
+	hello2<<<dimGrid, dimBlock>>>(gpu_block);
 
 	/* Free the arrays on the GPU as now we're done with them */
 	cudaMemcpy( cpu_block, gpu_block, ARRAY_SIZE_IN_BYTES, cudaMemcpyDeviceToHost );
-	cudaMemcpy( cpu_thread, gpu_thread, ARRAY_SIZE_IN_BYTES, cudaMemcpyDeviceToHost );
 	cudaFree(gpu_block);
-	cudaFree(gpu_thread);
 
 	/* Iterate through the arrays and print */
 	for(unsigned int i = 0; i < ARRAY_SIZE; i++)
 	{
-		printf("Thread: %2u - Block: %2u\n",cpu_thread[i],cpu_block[i]);
+		printf("Calculated Thread: - Block: %2u\n",cpu_block[i]);
 	}
 }
 
 int main()
 {
 	main_sub0();
+	main_sub1();
 
 	return EXIT_SUCCESS;
 }
