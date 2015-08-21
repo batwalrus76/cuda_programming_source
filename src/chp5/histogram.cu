b96ca381334fe5cadc2473b35fe365191c36eb31
pascact1 <Chancellor.Pascale@jhuapl.edu>
2015-08-21 19:08:58 -0400

    histogram is now working

--- src/chp5/histogram.cu
+++ src/chp5/histogram.cu
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Each thread writes to one block of 256 elements of global memory and contends for write access */
+#define ARRAY_SIZE 256
+#define ARRAY_SIZE_IN_BYTES (sizeof(unsigned int) * (ARRAY_SIZE))
 
-__global__ void myhistogram256kernel_01(const unsigned char const * d_hist_data,
-		unsigned int * const d_bin_data) {
+unsigned int cpu_block[ARRAY_SIZE];
+
+/* Each thread writes to one block of 256 elements of global memory and contends for write access */
+__global__ void myhistogram256kernel_01(unsigned int * hist_data,
+		unsigned int * bin_data) {
+	for(unsigned int i = 0; i < ARRAY_SIZE; i++)
+	{
+		bin_data[i] = 0;
+	}
+
 	/* Work out our thread id */
-	const unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-	const unsigned int idy = (blockIdx.y * blockDim.y) + threadIdx.y;
-	const unsigned int tid = idx + idy * blockDim.x * gridDim.x;
+	unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+	unsigned int idy = (blockIdx.y * blockDim.y) + threadIdx.y;
+	unsigned int tid = ((gridDim.x * blockDim.x) * idy) + idx;
 
 	/* Fetch the data value */
-	const unsigned char value = d_hist_data[tid];
-	atomicAdd(&(d_bin_data[value]), 1);
+	const unsigned char value = hist_data[tid];
+	atomicAdd(&(bin_data[value]), 1);
 }
 
 /* Each read is 4 bytes, not one, 32 x 4 = 128 byte reads */
-__global__ void myhistogram256kernel_02(const unsigned char const * d_hist_data,
-		unsigned int * const d_bin_data) {
+__global__ void myhistogram256kernel_02(unsigned int * d_hist_data,
+		unsigned int *d_bin_data) {
 	/* Work out our thread id */
 	const unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
 	const unsigned int idy = (blockIdx.y * blockDim.y) + threadIdx.y;
@@ -34,8 +43,8 @@ __global__ void myhistogram256kernel_02(const unsigned char const * d_hist_data,
 __shared__ unsigned int d_bin_data_shared[256];
 
 /* Each read is 4 bytes, not one, 32 x 4 = 128 byte reads */
-__global__ void myhistogram256kernel_03(const unsigned char const * d_hist_data,
-		unsigned int * const d_bin_data) {
+__global__ void myhistogram256kernel_03(unsigned int * d_hist_data,
+		unsigned int * d_bin_data) {
 	/* Work out our thread id */
 	const unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
 	const unsigned int idy = (blockIdx.y * blockDim.y) + threadIdx.y;
@@ -63,8 +72,8 @@ __global__ void myhistogram256kernel_03(const unsigned char const * d_hist_data,
 }
 
 /* Each read is 4 bytes, not one, 32 x 4 = 128 byte reads */
-__global__ void myhistogram256kernel_07(const unsigned int const * d_hist_data,
-		unsigned int * const d_bin_data, unsigned int N) {
+__global__ void myhistogram256kernel_04(unsigned int * d_hist_data,
+		unsigned int * d_bin_data, unsigned int N) {
 	/* Work out our thread id */
 	const unsigned int idx = (blockIdx.x * (blockDim.x * N)) + threadIdx.x;
 	const unsigned int idy = (blockIdx.y * blockDim.y) + threadIdx.y;
@@ -91,7 +100,118 @@ __global__ void myhistogram256kernel_07(const unsigned int const * d_hist_data,
 	atomicAdd(&(d_bin_data[threadIdx.x]), d_bin_data_shared[threadIdx.x]);
 }
 
-int main() {
+void generate_random_pointers(unsigned int * data)
+{
+	for(unsigned int i = 0; i < ARRAY_SIZE; i++)
+	{
+		unsigned int randNumber = rand() % ARRAY_SIZE;
+		data[i] = randNumber;
+	}
+}
+
+void perform_histogram_kernel(void (* kernel_function)(unsigned int *, unsigned int *))
+{
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	const unsigned int num_blocks = ARRAY_SIZE/32;
+	const unsigned int num_threads = ARRAY_SIZE/num_blocks;
+
+	/* Declare pointers for GPU based params */
+	unsigned int *hist_data;
+	unsigned int *bin_data;
+
+	cudaMalloc(&hist_data, ARRAY_SIZE_IN_BYTES);
+	cudaMalloc(&bin_data, ARRAY_SIZE_IN_BYTES);
+	generate_random_pointers(cpu_block);
+
+	cudaMemcpy( hist_data, cpu_block, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice );
+
+	/* Execute our kernel */
+	cudaEventRecord(start);
+	kernel_function<<<num_blocks, num_threads>>>(hist_data, bin_data);
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	printf("Time elapsed: %fn\n", milliseconds);
+
+	cudaMemcpy(cpu_block, bin_data, ARRAY_SIZE_IN_BYTES, cudaMemcpyDeviceToHost );
+	cudaFree(hist_data);
+	cudaFree(bin_data);
+
+	/* Iterate through the arrays and print */
+	for(unsigned int i = 0; i < ARRAY_SIZE; i++)
+	{
+		printf("histogram[%2u]=%2u\n",i,cpu_block[i]);
+	}
+}
+
+void perform_histogram_kernel_N(void (* kernel_function)(unsigned int *, unsigned int *, unsigned int), unsigned int N)
+{
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	const unsigned int num_blocks = ARRAY_SIZE/32;
+	const unsigned int num_threads = ARRAY_SIZE/num_blocks;
+
+	/* Declare pointers for GPU based params */
+	unsigned int *hist_data;
+	unsigned int *bin_data;
+
+	cudaMalloc(&hist_data, ARRAY_SIZE_IN_BYTES);
+	cudaMalloc(&bin_data, ARRAY_SIZE_IN_BYTES);
+	generate_random_pointers(cpu_block);
+
+	cudaMemcpy( hist_data, cpu_block, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice );
+
+	/* Execute our kernel */
+	cudaEventRecord(start);
+	kernel_function<<<num_blocks, num_threads>>>(hist_data, bin_data, N);
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	printf("Time elapsed: %fn\n", milliseconds);
+
+	cudaMemcpy(cpu_block, bin_data, ARRAY_SIZE_IN_BYTES, cudaMemcpyDeviceToHost );
+	cudaFree(hist_data);
+	cudaFree(bin_data);
+
+	/* Iterate through the arrays and print */
+	for(unsigned int i = 0; i < ARRAY_SIZE; i++)
+	{
+		printf("histogram[%2u]=%2u\n",i,cpu_block[i]);
+	}
+}
+void main_sub0()
+{
+//	cudaEvent_t start, stop;
+//	cudaEventCreate(&start);
+//	cudaEventCreate(&stop);
+	printf("myhistogram256kernel_01\n");
+//	cudaEventRecord(start);
+	perform_histogram_kernel(&myhistogram256kernel_01);
+//	cudaEventRecord(stop);
+//	cudaEventSynchronize(stop);
+//	float milliseconds = 0;
+//	cudaEventElapsedTime(&milliseconds, start, stop);
+//	printf("Time elapsed: %fn\n", milliseconds);
+
+	printf("myhistogram256kernel_02\n");
+	perform_histogram_kernel(&myhistogram256kernel_02);
+
+	printf("myhistogram256kernel_03\n");
+	perform_histogram_kernel(&myhistogram256kernel_03);
+
+	printf("myhistogram256kernel_04\n");
+	perform_histogram_kernel_N(&myhistogram256kernel_04, 2);
+
+}
+
+int main()
+{
+	main_sub0();
 
 	return EXIT_SUCCESS;
 }
